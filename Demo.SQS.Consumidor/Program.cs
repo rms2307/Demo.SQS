a19c162d6b5747ae27022f29f99c31f2fd243072
Robson de Moraes Silva <rms2307@outlook.com>
2022-06-26 16:44:18 -0300

    First commit.

---
+++ Demo.SQS.Consumidor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace Demo.SQS.Consumidor
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var queueUrl = "https://sqs.sa-east-1.amazonaws.com/653342422270/demosqs";
+
+            Console.WriteLine("Iniciando...");
+
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new ReceiveMessageRequest
+            {
+                QueueUrl = queueUrl
+            };
+
+            while (true)
+            {
+                var response = await client.ReceiveMessageAsync(request);
+                foreach (var message in response.Messages)
+                {
+                    Console.WriteLine("Recebendo mensagem.");
+                    Console.WriteLine(message.Body);
+                    await client.DeleteMessageAsync(queueUrl, message.ReceiptHandle);
+                }
+            }
+        }
+    }
+}
---
+++ Demo.SQS.Fornecedor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace Demo.SQS.Fornecedor
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Iniciando...");
+
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            for (int i = 1; i <= 3; i++)
+            {
+                var request = new SendMessageRequest
+                {
+                    QueueUrl = "https://sqs.sa-east-1.amazonaws.com/653342422270/demosqs",
+                    MessageBody = $"Mensagem nº{i}"                    
+                };
+
+                Console.WriteLine($"Enviando mensagem {i} para fila.");
+                await client.SendMessageAsync(request);
+                Console.WriteLine("Mensagem enviada.");
+            }           
+        }
+    }
+}
